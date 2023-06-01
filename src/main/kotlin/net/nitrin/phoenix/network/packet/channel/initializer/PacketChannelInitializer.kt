1a1685b83617386c329d7c2a5eff44b7ed71f1d7
NitrinCloud <admin@nitrin.net>
2023-06-01 22:24:40 +0200

    Change class to sealed

--- src/main/kotlin/net/nitrin/phoenix/network/packet/channel/initializer/PacketChannelInitializer.kt
+++ src/main/kotlin/net/nitrin/phoenix/network/packet/channel/initializer/PacketChannelInitializer.kt
@@ -6,7 +6,7 @@ import net.nitrin.phoenix.network.NetworkUtils
 import net.nitrin.phoenix.network.packet.PacketManager
 import net.nitrin.phoenix.network.packet.channel.PacketChannelInboundHandler
 
-open class PacketChannelInitializer(private val packetManager: PacketManager): ChannelInitializer<Channel>() {
+sealed class PacketChannelInitializer(private val packetManager: PacketManager): ChannelInitializer<Channel>() {
 
     override fun initChannel(channel: Channel) {
         NetworkUtils.addDefaultPipeline(channel)
